2bcd50e4616ceab80bdf425345462e4146de4f21
Plugg <153133226+2backside@users.noreply.github.com>
2024-12-31 23:45:41 -0300

    Create Demon Vessel.lua

---
+++ Demon Vessel.lua
@@ -0,0 +1,14 @@
+local player = game:GetService("Players").LocalPlayer
+local replicatedData = player:WaitForChild("ReplicatedData")
+local innates = replicatedData:WaitForChild("innates")
+local TargetInnate = {
+    [1] = "Demon Vessel",
+}
+for numero, valor in pairs(TargetInnate) do
+    local valorObj = innates:FindFirstChild(tostring(numero))
+    if valorObj then
+        valorObj.Value = valor
+    else
+        warn("Valor '" .. numero .. "' desconhecido")
+    end
+end
