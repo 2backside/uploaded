d049f2d5c7cf381590ebeb2f984b888a48306152
Plugg <153133226+2backside@users.noreply.github.com>
2025-01-01 03:54:09 -0300

    Create Star Rage2.lua

---
+++ Star Rage2.lua
@@ -0,0 +1,29 @@
+local player = game:GetService("Players").LocalPlayer
+local replicatedData = player:WaitForChild("ReplicatedData")
+local innates = replicatedData:WaitForChild("innates")
+local TargetInnate = {
+    [2] = "Star Rage",
+}
+for numero, valor in pairs(TargetInnate) do
+    local valorObj = innates:FindFirstChild(tostring(numero))
+    if valorObj then
+        valorObj.Value = valor
+    else
+        warn("Valor '" .. numero .. "' desconhecido")
+    end
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/2backside/vencordgui/refs/heads/main/learn.lua"))()
+local player = game:GetService("Players").LocalPlayer
+local masteries = player.ReplicatedData and player.ReplicatedData:FindFirstChild("masteries")
+
+if masteries then
+    local infinity = masteries:FindFirstChild("Star Rage")
+    if infinity and infinity:FindFirstChild("level") then
+        infinity.level.Value = 500
+    else
+        warn("The path 'ReplicatedData.masteries.Infinity.level' was not found.")
+    end
+else
+    warn("The path 'ReplicatedData.masteries' was not found.")
+end
